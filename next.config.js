8ef26bfa010de8efa1a9100676884a8273cd3c66
InderjyotSingh <inderjyotsingh9@gmail.com>
2025-04-20 12:09:00 -0400

    Add next.config.js to confirm pages router usage

---
+++ next.config.js
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  swcMinify: true,
+  // appDir: false // uncomment ONLY if you previously used App Router
+};
+
+module.exports = nextConfig;
